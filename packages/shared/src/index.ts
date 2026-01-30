4768adc03c18bd7655b92a3e397624f8c42e4ec6
MD SAHIL HASNAIN <mdsahil1631@gmail.com>
2026-01-30 21:43:28 +0530

    fixing build issues

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -7,5 +7,5 @@ export * from "./config";
 // Utils
 export * from "./utils/audio";
 export * from "./utils/dateUtils";
-export * from "./utils/durationFilter";
+export { filterNaatsByDuration } from "./utils/durationFilter";
 export * from "./utils/formatters";
